f4b43f259c354baf26b9a86566f74486e09f5568
Larry Hawelu <lhawelu@gmail.com>
2021-05-10 10:01:02 -0700

    adding user auth and user seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_211322) do
+ActiveRecord::Schema.define(version: 2021_05_07_213826) do
 
   create_table "flights", force: :cascade do |t|
     t.string "origin"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_05_07_211322) do
     t.string "username"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
